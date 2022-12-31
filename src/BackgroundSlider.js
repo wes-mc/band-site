9ded8bb889bdb30804e2c56a2f1e001fdb65b7f6
Wesley McCollam <wesley.mccollam@outlook.com>
2022-12-31 03:33:46 -0500

    background-slider working now

--- src/BackgroundSlider.js
+++ src/BackgroundSlider.js
@@ -1,13 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react'
-import './backgroundslider.css'
-import imageSlide from './data'
+import { SocialIcon } from 'react-social-icons';
 
 import img1 from './images/bandgroup.jpg'
 import img2 from './images/calebgoofy.jpg'
 import img3 from './images/drumpov.jpg'
 
 const BackgroundSlider = () => {
-
   const images = [img1, img2, img3];
 
   const [currentImage, setCurrentImage] = useState(0)
@@ -30,18 +28,26 @@ const BackgroundSlider = () => {
       clearInterval(sliderInterval);
     };
   });
+
   return (
     <div className="imgWrapper">
-      {images.map((img, index) => {
-        return (
-          <img
-            src={img}
-            className={
-              index === currentImage ? "imageActive homeImage" : "image"
-            }
-          />
-        );
-      })}
+      <div className="App" style={{ backgroundImage: `url(${images[currentImage]})` }}>
+        <header>
+          <ul className='ul-header'>
+            <SocialIcon url="https://www.facebook.com/people/Pioneer-Palace/100086961293325/" target="_blank" network='facebook' style={{ height: 50, width: 50, marginRight: 10 }} fgColor="#ffffff" />
+            <SocialIcon url="https://www.instagram.com/pioneerpalace/?hl=en" target="_blank" network='instagram' style={{ height: 50, width: 50, marginRight: 10 }} fgColor="#ffffff" />
+            <SocialIcon url="mailto:kylemarlow95@gmail.com?subject=PioneerPalace" network='mailto' style={{ height: 50, width: 50 }} fgColor="#ffffff" />
+          </ul>
+        </header>
+        <main>
+          <h1 className='main-text'>Pioneer Palace</h1>
+          <ul className='ul-menu'>
+            <li><a href="/">HOME</a></li>
+            <li><a>ABOUT</a></li>
+            <li><a>CONTACT</a></li>
+          </ul>
+        </main>
+      </div>
     </div>
   );
 };
