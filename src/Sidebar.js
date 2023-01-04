1c9cd38b837fa039757f49c4c86d800c698835a8
Wesley McCollam <wesley.mccollam@outlook.com>
2023-01-04 18:01:47 -0500

    changes for go-live

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -18,14 +18,27 @@ const Sidebar = () => {
     return (
         <div>
             <Menu>
-                <a name="Home" className="menu-item" href="/">
-                    Home
-                </a>
                 <a name="About" onClick={showModal} className="menu-item">
                     About
                 </a>
+                <a name="Contact" href="mailto:contact@pioneerpalaceband.com?Subject=Inquiry" className="menu-item">
+                    Contact
+                </a>
             </Menu>
-            <Modal title="About Us" zIndex={2000} className='modal' open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <Modal title="About Us" zIndex={2000} className='modal' open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={[
+                <Button key="back" onClick={handleCancel}>
+                    Cancel
+                </Button>,
+                <Button
+                    key="link"
+                    target='_blank'
+                    href="https://calendly.com"
+                    type="primary"
+                    onClick={handleOk}
+                >
+                    Book Now!
+                </Button>,
+            ]}>
                 <p>A local band from Spartanburg, SC ready to fulfill live music needs for your venue. Example venues include bars, parties, weddings, etc! Contact us on any platform seen on this site for pricing and availability. We hope to hear from you soon!</p>
             </Modal>
         </div>
