b46e3719d137b31cea10568a0f6fec8541d9b2f2
Wesley McCollam <wesley.mccollam@outlook.com>
2023-01-07 02:33:15 -0500

    general layout for audio enablement

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import './App.css';
 import bGvideo from './videos/short.MOV'
 import Sidebar from './Sidebar'
 import Socials from './Socials'
-import { Button } from 'antd'
+import { Button, Switch } from 'antd'
 import ReactGA from 'react-ga';
-const TRACKING_ID = "G-JLH78CCXBN";
 
-function App() { 
+const TRACKING_ID = "G-JLH78CCXBN";
+const LoadVideo = () => {
+    return (
+        <video autoPlay loop muted playsInline className="video" src={bGvideo} type="video/mp4"></video>
+    )
+}
+
+function App() {   
     ReactGA.initialize(TRACKING_ID);
     ReactGA.pageview(window.location.pathname + window.location.search);    
     return (
         <div>
-            <video autoPlay loop muted playsInline className="video"><source src={bGvideo} type="video/mp4" /> </video>
+            <LoadVideo />
             <header>
-            <meta name="viewport" content="width=device-width, intial-scale=1.0" />
-            <ul className='ul-header'>
-                <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-                <Socials />
-            </ul>
+                <meta name="viewport" content="width=device-width, intial-scale=1.0" />
+                <ul className='ul-header'>
+                    <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+                    <Socials />
+                </ul>
             </header>
             <main>
+            <div className='audio'>
+                <Switch checkedChildren="Audio Enabled" unCheckedChildren="Audio Disabled"/>
+            </div>
             <h1 className='main-text'>Pioneer Palace</h1>
             <h2 className='main-button'>
-                <Button target='_blank' href='https://calendly.com/pioneerpalace/pioneer-palace-gig' size='large' style={{ fontSize: "xxx-large", paddingTop: "auto", paddingBottom: "auto", marginTop: "10px", height: "auto", width: "auto" }} ghost>Click to book!</Button>
+                <Button target='_blank' href='https://calendly.com/pioneerpalace/pioneer-palace-gig' size='large' style={{ fontSize: "xx-large", paddingTop: "auto", paddingBottom: "auto", marginTop: "10px", height: "auto", width: "auto" }} ghost>Click to book!</Button>
             </h2>
             </main>
             <footer className='footer'>This site is built and managed by Pioneer Palace</footer>
